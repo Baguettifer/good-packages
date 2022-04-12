499dd64fc11a0126beeee022b731124e2d363d80
Baguettifer <deaudneyaaron@gmail.com>
2022-04-12 21:55:05 +1000

    i use arch btw

---
+++ btw.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo pacman -S grub efibootmgr networkmanager network-manager-applet mtools dosftools reflector git base-devel linux-headers bluez bluez-utils cups xdg-utils xdg-user-dirs
